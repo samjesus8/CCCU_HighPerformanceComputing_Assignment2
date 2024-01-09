96de0abbefa1031789ec38945b43a12da2f78b8a
Samuel Jesuthas <samueljesuthas27@gmail.com>
2024-01-09 00:45:27 +0000

    App.java changed inputs to global values

--- src/App.java
+++ src/App.java
@@ -15,24 +15,36 @@ public class App {
                             "1. Run /w No Threading \n" +
                             "2. Run /w Threading \n" +
                             "3. Run using Thread Pool \n" +
-                            "4. Exit Program");
+                            "4. Verify Threaded vs Non-Threaded \n" +
+                            "5. Exit Program");
         int input = threadingInputOption.nextInt();
 
         long startTime, endTime; //These 2 variables will be used to show how long it took for our methods to execute
+        
+        matrixEngine MatrixEngine = new matrixEngine();
+        var startingMatrix = MatrixEngine.GenerateBaseMatrixes();
 
         if (input == 1) {
             startTime = System.currentTimeMillis();
-            performMatrixMultiplicationNoThreading();
+            performMatrixMultiplicationNoThreading(startingMatrix.matrix1, startingMatrix.matrix2);
             endTime = System.currentTimeMillis();
         } else if (input == 2) {
             startTime = System.currentTimeMillis();
-            performMatrixMultiplicationThreaded();
+            performMatrixMultiplicationThreaded(startingMatrix.matrix1, startingMatrix.matrix2);
             endTime = System.currentTimeMillis();
         } else if (input == 3) {
             startTime = System.currentTimeMillis();
             performMatrixMultiplicationThreadPool();
             endTime = System.currentTimeMillis();
         } else if (input == 4) {
+            startTime = System.currentTimeMillis();
+            var nonThreaded = performMatrixMultiplicationNoThreading(startingMatrix.matrix1, startingMatrix.matrix2);
+            var threaded = performMatrixMultiplicationThreaded(startingMatrix.matrix1, startingMatrix.matrix2);
+
+            verifyResult(threaded, nonThreaded);
+
+            endTime = System.currentTimeMillis();
+        } else if (input == 5) {
             System.out.println("Exiting Program...");
             threadingInputOption.close();
             return; // Exit the program
@@ -48,21 +60,17 @@ public class App {
         threadingInputOption.close();
     }
 
-    private static void performMatrixMultiplicationNoThreading() {
-        //Initialize the main Matrix generation class
+    private static long[][] performMatrixMultiplicationNoThreading(long[][] matrix1, long[][] matrix2) {
         var MatrixEngine = new matrixEngine();
-
-        //Execute method to generate 2 basic matrixes
-        var matrixes = MatrixEngine.GenerateBaseMatrixes();
 
         //Display the 2 starting matrixes which will be multiplied together to give our first iteration
         System.out.println("Matrix 1: \n");
-        printMatrixPreview(matrixes.matrix1, 10, 10); //We will only display the first 10x10 portion of the matrix
+        printMatrixPreview(matrix1, 10, 10); //We will only display the first 10x10 portion of the matrix
         System.out.println("Matrix 2: \n");
-        printMatrixPreview(matrixes.matrix2, 10, 10);
+        printMatrixPreview(matrix2, 10, 10);
 
         //Executing the multiplication method to get our first iteration
-        var result1 = MatrixEngine.multiplyMatrices(matrixes.matrix1, matrixes.matrix2);
+        var result1 = MatrixEngine.multiplyMatrices(matrix1, matrix2);
         System.out.println("1st Multiplication /w No Threading: \n");
         printMatrixPreview(result1, 10, 10);
 
@@ -83,23 +91,23 @@ public class App {
         var result3 = MatrixEngine.multiplyMatrices(result2, thirdIterationMatrix);
         System.out.println("3rd Multiplication /w No Threading: \n");
         printMatrixPreview(result3, 10, 10);
+
+        //Return the final result
+        return result3;
     }
 
-    private static void performMatrixMultiplicationThreaded() {
+    private static long[][] performMatrixMultiplicationThreaded(long[][] matrix1, long[][] matrix2) {
         //Initialize Class
         var MatrixEngine = new matrixEngine();
 
-        //Generate base matrixes
-        var matrixes = MatrixEngine.GenerateBaseMatrixes();
-
         //Print out the first 10x10 portion of each matrix
         System.out.println("Matrix 1: \n");
-        printMatrixPreview(matrixes.matrix1, 10, 10);
+        printMatrixPreview(matrix1, 10, 10);
         System.out.println("Matrix 2: \n");
-        printMatrixPreview(matrixes.matrix2, 10, 10);
+        printMatrixPreview(matrix2, 10, 10);
 
         //Multiplication 1 (Using 4 threads)
-        var result1 = MatrixEngine.multiplyMatricesThreaded(matrixes.matrix1, matrixes.matrix2, 4);
+        var result1 = MatrixEngine.multiplyMatricesThreaded(matrix1, matrix2, 4);
         System.out.println("1st Multiplication /w Threading: \n");
         printMatrixPreview(result1, 10, 10);
 
@@ -118,6 +126,9 @@ public class App {
         var result3 = MatrixEngine.multiplyMatricesThreaded(result2, thirdIterationMatrix, 4);
         System.out.println("3rd Multiplication /w Threading: \n");
         printMatrixPreview(result3, 10, 10);
+
+        //Return the final result
+        return result3;
     }
 
     private static void performMatrixMultiplicationThreadPool() {
@@ -136,5 +147,26 @@ public class App {
         }
 
         System.out.println();
+    }
+
+    private static void verifyResult(long[][] result, long[][] goldStandardResult) {
+        boolean verificationPassed = true;
+        for (int i = 0; i < 1000; i++) {
+            for (int j = 0; j < 1000; j++) {
+                if (result[i][j] != goldStandardResult[i][j]) {
+                    verificationPassed = false;
+                    break;
+                }
+            }
+            if (!verificationPassed) {
+                break;
+            }
+        }
+
+        if (verificationPassed) {
+            System.out.println("Verification successful! Results match the gold standard.");
+        } else {
+            System.err.println("Verification failed! Results differ.");
+        }
     }
 }
